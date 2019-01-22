4359d48d3cbc01811ed658b533e7fee295eafde7
tki <tki.wroc@gmail.com>
2019-01-22 17:44:22 +0100

    add PassField

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import Button    from './button'
 import Icon      from './icon'
 import Indicator from './indicator'
+import PassField from './pass_field'
 
-export { Button, Icon, Indicator };
+export { Button, Icon, Indicator, PassField };
