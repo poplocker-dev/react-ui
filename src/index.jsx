7d49289a17e09b2d7fb6e84f6b65d43d483ebd36
tki <tki.wroc@gmail.com>
2019-02-13 10:20:57 +0100

    update bouncing and add it to index

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import Button    from './button'
 import Icon      from './icon'
 import Indicator from './indicator'
 import PassField from './pass_field'
+import Bouncing  from './bouncing'
 
 import 'css/variables.css'
 
-export { Button, Icon, Indicator, PassField };
+export { Button, Icon, Indicator, PassField, Bouncing };
